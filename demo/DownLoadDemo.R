4f16d773721c40c3061ee25ad968dba3bca3a9d9
Steven Mosher <moshersteven@gmail.com>
2012-07-21 00:00:00 +0000

    version 1.5

--- demo/DownLoadDemo.R
+++
@@ -1,8 +0,0 @@
- if (!file.exists(DAILY.QA.DIRECTORY)) dir.create(DAILY.QA.DIRECTORY)
- if (!file.exists(DAILY.DATA.DIRECTORY)) dir.create(DAILY.DATA.DIRECTORY)
- if (!file.exists(DAILY.FILES.DIRECTORY)) dir.create(DAILY.FILES.DIRECTORY)
- if (!file.exists(MONTHLY.DATA.DIRECTORY)) dir.create(MONTHLY.DATA.DIRECTORY)
- cntryFilename     <- downloadCountry()
- inventoryFilename <- downloadDailyInventory()
- metadataFilename  <- downloadDailyMetadata()
- 
